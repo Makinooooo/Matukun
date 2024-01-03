9e77fa8bb01f29fda5b2f16b358e54c5ee511acd
Makinooooo <it232254@trident.ac.jp>
2024-01-03 17:31:32 +0900

    簡易プレイヤーの作成（ジャンプアニメーション未実装）

---
+++ Assets/Scenes/Makino Scene/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    Animator animator;
+    float jumpForce = 400.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        this.rigid2D = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ジャンプ
+        if (Input.GetKeyDown(KeyCode.Space) &&
+            this.rigid2D.velocity.y == 0)
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        // 左右移動
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        // 速度
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        // スピード制限
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        // 向きに応じて反転
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        // 速度に応じてアニメーション
+        this.animator.speed = speedx / 2.0f;
+    }
+}
