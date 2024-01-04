905ef55a17dd4b05320e436baa1eba7403ee0ac3
Makinooooo <it232254@trident.ac.jp>
2024-01-04 17:28:52 +0900

    簡易プレイヤーの完成

--- Assets/Scenes/Makino Scene/PlayerController.cs
+++ Assets/Scenes/Makino Scene/PlayerController.cs
@@ -25,6 +25,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) &&
             this.rigid2D.velocity.y == 0)
         {
+            this.animator.SetTrigger("JumpTrigger");
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
 
@@ -49,6 +50,14 @@ public class PlayerController : MonoBehaviour
         }
 
         // 速度に応じてアニメーション
-        this.animator.speed = speedx / 2.0f;
+        if (this.rigid2D.velocity.y == 0)
+        {
+            this.animator.speed = speedx / 2.0f;
+        }
+        else
+        {
+            this.animator.speed = 1.0f;
+        }
+        
     }
 }
